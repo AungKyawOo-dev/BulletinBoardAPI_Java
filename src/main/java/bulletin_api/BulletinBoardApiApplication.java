409e1733f92433f41410b78d32ff8114ac912bb4
Aung Kyaw Oo <scm.aungkyawoo@gmail.com>
2022-08-16 13:55:41 +0630

    first commit

---
+++ src/main/java/bulletin_api/BulletinBoardApiApplication.java
@@ -0,0 +1,13 @@
+package bulletin_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinBoardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulletinBoardApiApplication.class, args);
+	}
+
+}
